a8d81acdd7c660c43a37775ffe7b873528980036
Sushil Kumar <sushilkumar567895@gmail.com>
2025-04-09 16:32:26 +0530

    files were added

---
+++ README.md
@@ -0,0 +1,14 @@
+# Calculator App
+
+A user-friendly calculator app designed for basic arithmetic operations, including addition, subtraction, multiplication, division, and modulus. It offers a clean design with smooth error handling to deliver a seamless user experience.
+
+**Code Output:**
+Please click [here](https://sushilkumar567.github.io/6-calculator-app/) to view the live code output.
+
+## Tech Stack:
+
+**React:** Handles the user interface with functional components and state management via useState.
+
+**JavaScript:** Manages the calculator logic, input handling, and expression evaluation.
+
+**CSS:** Custom styling applied through App.css for layout, button styling, and responsive design.
---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [expression, setExpression] = useState("");
+
+  const handleClick = (value) => {
+    try {
+      if (value === "=") {
+        setExpression(eval(expression).toString());
+      } else if (value === "AC") {
+        setExpression("");
+      } else if (value === "Del") {
+        setExpression(expression.slice(0, -1));
+      } else {
+        setExpression(expression + value);
+      }
+    } catch {
+      setExpression("Error");
+    }
+  };
+
+  const buttons = [
+    "AC",
+    "Del",
+    "/",
+    "*",
+    "7",
+    "8",
+    "9",
+    "-",
+    "4",
+    "5",
+    "6",
+    "+",
+    "1",
+    "2",
+    "3",
+    "=",
+    "0",
+    "00",
+    ".",
+  ];
+
+  const isOperator = (val) =>
+    ["AC", "Del", "+", "-", "*", "/", "="].includes(val);
+
+  return (
+    <div className="calculator-container">
+      <div className="calculator">
+        <input id="display" className="display" value={expression} readOnly />
+        <div className="buttons">
+          {buttons.map((btn, index) => (
+            <button
+              key={index}
+              onClick={() => handleClick(btn)}
+              className={`btn ${btn == "=" ? "equals-btn" : ""}
+              ${isOperator(btn) ? "operator-btn" : ""}`}
+            >
+              {btn}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
